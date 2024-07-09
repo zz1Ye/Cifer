d4d8e45e57e4d696e710b5dc6c298f460defcfed
zzYe <zzy_nic@126.com>
2024-07-09 22:03:15 +0800

    update item

---
+++ dao/base.py
@@ -0,0 +1,118 @@
+import json
+import os
+from typing import Generator, List
+
+import ijson
+
+
+class BaseDao:
+    def __init__(self, fpath: str):
+        self.fpath = fpath
+
+    def exist(self) -> bool:
+        return os.path.isfile(self.fpath)
+
+
+class JsonDao(BaseDao):
+    def __init__(self, fpath: str):
+        super().__init__(fpath)
+
+    def create(self) -> bool:
+        try:
+            os.makedirs(os.path.dirname(self.fpath), exist_ok=True)
+            with open(self.fpath, 'w') as _:
+                pass
+
+        except Exception as e:
+            print(f"Error creating file: {e}")
+            return False
+
+        return True
+
+    def load(self, batch_size: int) -> Generator[List[dict], None, None]:
+        with open(self.fpath, 'r', encoding='utf-8') as file:
+            if file.read(1) == '':
+                return
+            file.seek(0)
+
+            objects = ijson.items(file, 'item')
+
+            buffer = []
+            for obj in objects:
+                buffer.append(obj)
+
+                if len(buffer) == batch_size:
+                    yield buffer
+                    buffer = []
+
+            if buffer:
+                yield buffer
+
+    def insert(self, data: dict) -> bool:
+        rows = [
+            r for batch in self.load(batch_size=1024) for r in batch
+        ]
+        rows.append(data)
+
+        with open(self.fpath, 'w', encoding='utf-8') as file:
+            json.dump(
+                rows,
+                file, ensure_ascii=False, indent=4
+            )
+
+        return True
+
+    def delete(self, filter_params: dict) -> bool:
+        rows = [
+            r for batch in self.load(batch_size=1024) for r in batch
+            if not all(
+                r.get(k) == v
+                for k, v in filter_params.items()
+            )
+        ]
+
+        with open(self.fpath, 'w', encoding='utf-8') as file:
+            json.dump(
+                rows,
+                file, ensure_ascii=False, indent=4
+            )
+
+        return True
+
+    def query(self, filter_params: dict, limit: int = sys.maxsize) -> List[dict]:
+        rows = []
+
+        for batch in self.load(batch_size=1024):
+            rows.extend([
+                r for r in batch
+                if all(
+                    r.get(k) == v
+                    for k, v in filter_params.items()
+                )
+            ])
+
+            if len(rows) >= limit:
+                break
+
+        return rows[:limit]
+
+    def update(self, filter_params: dict, new_data: dict) -> bool:
+        rows = [
+            r for batch in self.load(batch_size=1024) for r in batch
+        ]
+        for i in range(len(rows)):
+            if all(
+                rows[i].get(k) == v
+                for k, v in filter_params.items()
+            ):
+                rows[i].update(
+                    {k: v for k, v in new_data.items() if k in rows[i]}
+                )
+
+        with open(self.fpath, 'w', encoding='utf-8') as file:
+            json.dump(
+                rows,
+                file, ensure_ascii=False, indent=4
+            )
+
+        return True
--- item/evm/tx.py
+++ item/evm/tx.py
@@ -6,3 +6,41 @@
 @Author : zzYe
 
 """
+from pydantic import BaseModel
+
+
+class Transaction(BaseModel):
+    hash: str
+    block_hash: str
+    block_number: int
+    from_address: str
+    to_address: str
+    value: int
+    timestamp: int
+    gas: int
+    gas_price: int
+    input: str
+    nonce: str
+    type: str
+    chain_id: int
+
+
+class Trace(BaseModel):
+    action: dict
+    block_hash: str
+    block_number: int
+    result: dict
+    subtraces: int
+    trace_address: list
+    transaction_hash: str
+    transaction_position: int
+    type: str
+
+
+class TraceAction(BaseModel):
+    from_address: str
+    to_address: str
+    call_type: str
+    gas: int
+    input: str
+    value: int
