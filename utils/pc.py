ca2f66b48d567a529f307b5a38dfa9cb3658706f
zzYe <zzy_nic@126.com>
2024-07-12 10:29:13 +0800

    update pc

--- utils/pc.py
+++ utils/pc.py
@@ -9,6 +9,7 @@
 import asyncio
 import hashlib
 from enum import Enum
+from queue import Queue
 from typing import List
 
 from dao.meta import Dao
@@ -91,45 +92,25 @@ class Job:
 
 
 class PC:
+    def __init__(self, source: Queue[Job], maxsize: int = 64):
+        self.sq = source
+        self.jq = asyncio.Queue(maxsize=maxsize)
 
-    queue = asyncio.Queue()
-
-    def __init__(self):
-        pass
+    async def producer(self):
+        while True and self.sq.qsize() != 0:
+            job = self.sq.get()
+            await self.jq.put(job)
 
     async def consumer(self):
         while True:
-            item = await self.queue.get()
-            self.queue.task_done()
+            if self.sq.empty() and self.jq.empty():
+                break
+            job = await self.jq.get()
+            await job.run()
 
-    async def producer(num_workers, jobs):
-        for i in range(num_workers):
-            worker = asyncio.create_task(consumer(self.queue))
-        # 将作业分配给消费者
-        for job in jobs:
-            await queue.put(job)
-        # 通知所有消费者没有更多的作业
-        for _ in range(num_workers):
-            queue.put(None)
-        await queue.join()  # 等待所有任务完成
-
-    async def pc(self):
-        # 假设 source_of_tasks 是一个异步迭代器，不断产生新的爬虫任务
-        source_of_tasks = some_async_iterable_of_transaction_hashes()
-
-        # 创建生产者任务
-        producer_task = asyncio.create_task(producer(queue, session, source_of_tasks))
-
-        # 创建消费者任务
-        consumers = [asyncio.create_task(consumer(queue, session)) for _ in range(5)]
-
-        # 等待生产者任务完成
-        await producer_task
-
-        # 等待队列中的所有任务被处理
-        await queue.join()
-
-        # 取消消费者任务
-        for consumer in consumers:
-            consumer.cancel()
+    async def run(self):
+        p_worker = asyncio.create_task(self.producer())
+        c_worker = asyncio.create_task(self.consumer())
+        workers = [p_worker, c_worker]
+        await asyncio.gather(*workers)
 
